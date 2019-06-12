1ecd147123042b4653a7f64f54f2b1f222d455b5
Mathew Zummo <mzummo@gmail.com>
2019-06-12 02:43:41 -0400

    initial datatable testing
    Signed-off-by: Mathew Zummo <mzummo@gmail.com>

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,8 +9,10 @@ window._ = require('lodash');
 try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
-
     require('bootstrap');
+    //window.dt = require( 'datatables.net-bs4' )();
+    window.dt = require( 'datatables.net-bs4' )( window, $ );
+
 } catch (e) {}
 
 /**
